697f3009598e1339de30a956587797c531c30baf
Michael Rozenblat <mike.rozen1@gmail.com>
2018-09-17 17:29:26 +0300

    Initial Commit

---
+++ Zooz/Zooz.h
@@ -0,0 +1,19 @@
+//
+//  Zooz.h
+//  Zooz
+//
+//  Created by Michael Rozenblat on 17/09/2018.
+//  Copyright © 2018 Michael Rozenblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Zooz.
+FOUNDATION_EXPORT double ZoozVersionNumber;
+
+//! Project version string for Zooz.
+FOUNDATION_EXPORT const unsigned char ZoozVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Zooz/PublicHeader.h>
+
+
---
+++ ZoozTests/ZoozTests.m
@@ -0,0 +1,39 @@
+//
+//  ZoozTests.m
+//  ZoozTests
+//
+//  Created by Michael Rozenblat on 17/09/2018.
+//  Copyright © 2018 Michael Rozenblat. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZoozTests : XCTestCase
+
+@end
+
+@implementation ZoozTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
